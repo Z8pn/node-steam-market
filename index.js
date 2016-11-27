649990bd99a6b8fd9f9c02a88f501205dd476996
Z8pn <kryptoholyx@googlemail.com>
2016-11-28 00:15:02 +0100

    Update index.js

--- index.js
+++ index.js
@@ -23,22 +23,22 @@ SteamMarket.prototype.sellItem = function (item,callback){
         url: 'https://steamcommunity.com/market/sellitem/',
         headers: {
         	"Accept": "*/*",
-            'Origin': 'http://steamcommunity.com',
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.112 Safari/537.36',
-            'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-            'Referer': 'http://steamcommunity.com/my/inventory/',
-            'Accept-Encoding': 'gzip, deflate',
-            'Accept-Language': 'ru,en-US;q=0.8,en;q=0.6,en-AU;q=0.4'
-        },
-        form: {
-            "sessionid": this.sessionid,
-            "appid": item.appid,
-            "contextid": item.contextid,
-            "assetid": item.assetid,
-            "amount": 1,
-            "price": item.price
-        }
-    };
+		    'Origin': 'http://steamcommunity.com',
+		    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.112 Safari/537.36',
+		    'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+		    'Referer': 'http://steamcommunity.com/my/inventory/',
+		    'Accept-Encoding': 'gzip, deflate',
+		    'Accept-Language': 'ru,en-US;q=0.8,en;q=0.6,en-AU;q=0.4'
+		},
+		form: {
+		    "sessionid": this.sessionid,
+		    "appid": item.appid,
+		    "contextid": item.contextid,
+		    "assetid": item.assetid,
+		    "amount": 1,
+		    "price": item.price
+		     }
+		};
 		this._request(options, function(error, response, body){
 			var body = JSON.parse(body);
 			console.log(body);
